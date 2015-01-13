472cd230d1ab324ee031b01a51d58060dd189032
Derek Lai <dereklai.kog@gmail.com>
2015-01-13 14:34:19 -0800

    changed to allow it to work on different machines - use HOME dir

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'ubuntu'
-client_key               '/home/dlai/chef/chefsvr11-ostk/.chef/ubuntu.pem'
+client_key               '~/chef/chefsvr11-ostk/.chef/ubuntu.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/dlai/chef/chefsvr11-ostk/.chef/chef-validator.pem'
-chef_server_url          'https://chefsvr11:443'
-syntax_check_cache_path  '/home/dlai/chef/chefsvr11-ostk/.chef/syntax_check_cache'
+validation_key           '~/chef/chefsvr11-ostk/.chef/chef-validator.pem'
+chef_server_url          'https://169.53.134.148:443'
+syntax_check_cache_path  '~/chef/chefsvr11-ostk/.chef/syntax_check_cache'
 cookbook_path [ './cookbooks' ]
